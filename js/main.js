7b35667c39755e99d290da3999a9400c9b4a7eba
Farai <lameckfarai@gmail.com>
2017-03-10 12:55:55 +0200

    toggling on javascipt

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let mobileMenuButton=document.getElementById('mobile-menu-button');
+function toggleMobileMenu() {
+
+  alert('ouch');
+}
+  mobileMenuButton.addEventListener('click', toggleMobileMenu);
