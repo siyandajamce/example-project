ca937137f5568c34d89d4fe3273aa36df13dae82
Farai <lameckfarai@gmail.com>
2017-03-10 13:31:04 +0200

    toggling the menu button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let mobileMenuButton=document.getElementById('mobile-menu-button');
+let siteMenu=document.getElementById('site-menu');
 function toggleMobileMenu() {
 
-  alert('ouch');
+  if (!siteMenu.style.display) {
+    siteMenu.style.display='flex';
+  } else {
+    siteMenu.style.display="";
+  }
 }
   mobileMenuButton.addEventListener('click', toggleMobileMenu);
